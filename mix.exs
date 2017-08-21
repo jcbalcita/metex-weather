c10beb8f10244c23da6ab63177145b37e7262105
jbalcita <jbalcita@cj.com>
2017-08-20 19:40:43 -0700

    can fetch temperature of a given city

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Metex.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :metex,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      applications: [:httpoison],
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 0.13"},
+      {:json, "~> 1.0"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
