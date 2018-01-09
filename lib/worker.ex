58244fd200c056f233ac12cb33b833124236add1
jbalcita <jbalcita@cj.com>
2018-01-09 10:08:18 -0800

    "naive" worker uses loop for async calls

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,18 +1,40 @@
-defmodule Metex do
-  @moduledoc """
-  Documentation for Metex.
-  """
+defmodule Metex.Worker do
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Metex.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def loop do
+    receive do
+      {sender_pid, location} -> send(sender_pid, {:ok, temperature_of(location)})
+      _                      -> IO.puts "Unable to process this message"
+    end
+    loop()
   end
+
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp} -> "#{location}: #{temp} °C"
+      :error      -> "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{api_key}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> Poison.decode! |> compute_temperature
+  end
+
+  defp parse_response(_), do: :error
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  def api_key, do: Application.get_env(:metex, :api_key)
 end
